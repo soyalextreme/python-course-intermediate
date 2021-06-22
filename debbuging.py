d6038ef9a8dc6da1935f933ffa68db13db57a8b1
soyalextreme <soyalextreme@gmail.com>
2021-06-21 21:23:11 -0500

    Changes on the commit: [add] The exception handler for the challenge

--- debbuging.py
+++ debbuging.py
@@ -1,13 +1,31 @@
+class NegativeNumberNotValid(Exception):
+    pass
+
 def divisors(num):
     divisors = [i for i in range(1, num + 1) if num % i == 0]
-    print("pausing here")
     return divisors
 
 
 def run():
-    num = int(input("Ingresa un numero: "))
-    print(divisors(num))
-    print("Termino mi programa")
+    detected_errors = True
+    while detected_errors != False:
+        try:
+            num = int(input("Enter an entire number: "))
+            if num < 0:
+                raise NegativeNumberNotValid(
+                    "We do not accept negative numbers")
+            print(divisors(num))
+            detected_errors = False
+        except ValueError:
+            print("Please enter a number")
+        except NegativeNumberNotValid as err_negative:
+            print(err_negative)
+        finally:
+            if detected_errors:
+                print('Try again')
+            else:
+                break
+
 
 
 if __name__ == "__main__":
